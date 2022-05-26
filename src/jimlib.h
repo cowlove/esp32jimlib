09c14e29f1616fed629833e3d898d85c9d8b09d7
jim <jim@vheavy.com>
2022-05-26 10:31:23 -0700

    Move JStuff to jimlib

--- src/jimlib.h
+++ src/jimlib.h
@@ -17,6 +17,7 @@
 #include "WiFiUdp.h"
 #include "Wire.h"
 #include <OneWireNg.h>
+#include <OneWireNg_CurrentPlatform.h>
 #include <WiFiClientSecure.h>
 #include <PubSubClient.h>
 
@@ -32,6 +33,8 @@
 #include <soc/soc.h>
 #include <soc/rtc_cntl_reg.h>
 #include <mySD.h> // Add "EXCLUDE_DIRS=esp32-micro-sdcard" to Makefile if this breaks ESP8266 builds
+#include <utility>
+#include <regex>
 #endif //ESP32
 #endif // !UBUNTU
 
@@ -1003,7 +1006,7 @@ public:
 			return;
 		udp.beginPacket("255.255.255.255", 9000);
 		char b[128];
-		snprintf(b, sizeof(b), "%d %s %s " __DATE__ "   " __TIME__ "   %s ", 
+		snprintf(b, sizeof(b), "%08d %s %s " __DATE__ "   " __TIME__ "   %s ", 
 			(int)(millis() / 1000), WiFi.localIP().toString().c_str(), 
 			basename_strip_ext(__BASE_FILE__).c_str(), getMacAddress().c_str());
 		udp.write((const uint8_t *)b, strlen(b));
@@ -1129,6 +1132,14 @@ static inline float avgAnalogRead(int pin, int avg = 1024) {
 		bv += analogRead(pin);
 	}
 	return bv / avg;
+}
+
+static inline void bin2hex(const char *in, int len, char *out, int olen) {
+	len = min(len, olen / 2); 
+	for (int n = 0; n < len; n++) { 
+		sprintf(out + 2 * n, "%02x", in[n]);
+	}
+	out[2 * len] = '\0';
 }
 
 static inline int hex2bin(const char *in, char *out, int inLength) { 
@@ -1299,6 +1310,183 @@ int getLedPin() {
 	Serial.printf("MAC %s not found in getLedPin, defaulting to pin 2\n", mac.c_str());
 	return 2;
 }
+
+
+#ifdef ESP32 // TODO - this could move back to supporting ESP8266
+using namespace std;
+
+class CommandLineInterface { 
+	typedef std::function<string(const char *,std::smatch)> callback;
+	std::vector<std::pair<std::string, callback>> handlers;
+public:
+
+	CommandLineInterface() { 
+	}
+
+	void on(const char *pat, callback h) { 
+		handlers.push_back(std::pair<std::string, callback>(pat, h));
+	}
+	void on(const char *pat, std::function<void()> f) { 
+		on(pat, [f](const char *, std::smatch) { f(); return string(); });
+	}
+	void on(const char *pat, std::function<void(std::smatch)> f) { 
+		on(pat, [f](const char *, std::smatch m) { f(m); return string(); });
+	}
+	void on(const char *pat, std::function<void(const char *l)> f) { 
+		on(pat, [f](const char *l, std::smatch) { f(l); return string(); });
+	}
+	string process(const char *line) {
+		string rval = ""; 
+		if (strstr(line, "hooks")) { 
+			smatch dummy;
+			for(auto i = handlers.begin() + 1; i != handlers.end(); i++) { 
+				rval += i->first + " \t " + i->second("", dummy) + "\n";
+			}
+			return rval;
+		}
+		for(auto i = handlers.begin(); i != handlers.end(); i++) { 
+			std::smatch res;
+			std::regex exp((i->first).c_str());
+			std::string str = line;
+			if (std::regex_match(str, res, exp))  
+				rval += i->second(line, res);
+			}
+		return rval;
+	}
+	template<typename T>
+	void hookRaw(const char *pat, T* v) {
+		const char *fmt = formatOf<T>();
+		on(pat, [v,fmt](const char *, smatch m) { 
+			if (m.size() > 1) 
+				sscanf(m.str(1).c_str(), fmt, v);
+			return strfmt(fmt, *v);
+		});
+	}
+
+	template<typename T>
+	void hookVar(const char *l, T*p) {
+		std::string s = strfmt("set %s=(.*)", l);
+		hookRaw(s.c_str(), p);
+		s = strfmt("get %s", l);
+		hookRaw(s.c_str(), p);	
+	}
+
+	template<typename T> const char *formatOf();
+};
+
+template<> const char *CommandLineInterface::formatOf<float>() { return "%f"; }
+template<> const char *CommandLineInterface::formatOf<int>() { return "%i"; }
+
+template<>
+void CommandLineInterface::hookRaw<string>(const char *pat, string *v) {
+	on(pat, [v](const char *, smatch m) { 
+		if (m.size() > 1)
+			*v = m.str(1).c_str();
+		return (*v).c_str();
+	});
+}
+
+class JStuff {		
+	bool parseSerial;
+	std::function<void()> onConn = NULL;
+	LineBuffer lb;
+	bool debug = false;
+public:
+	CommandLineInterface cli;
+	JStuff(bool ps = true) : parseSerial(ps) {
+		cli.on("DEBUG", [this]() { debug = true;});
+	}
+	JimWiFi jw;
+	MQTTClient mqtt = MQTTClient("192.168.5.1", basename_strip_ext(__BASE_FILE__).c_str());
+	void run() { 
+		esp_task_wdt_reset();
+		jw.run(); 
+		mqtt.run(); 
+		while(parseSerial == true && Serial.available()) { 
+			lb.add(Serial.read(), [this](const char *l) {
+				string r = cli.process(l);
+				Serial.println(r.c_str());
+			});
+		}
+	}
+	void begin() { 
+		esp_task_wdt_init(60, true);
+		esp_task_wdt_add(NULL);
+
+		Serial.begin(921600, SERIAL_8N1);
+		Serial.println(__BASE_FILE__ " " GIT_VERSION);
+		getLedPin();
+
+		jw.onConnect([this](){
+			jw.debug = mqtt.active = (WiFi.SSID() == "ChloeNet");
+			Serial.printf("WiFi connected to %s\n", WiFi.SSID().c_str());
+			if (onConn != NULL) { 
+				onConn();
+			}
+		});
+		mqtt.setCallback([this](String t, String m) {
+			string r = cli.process(m.c_str());
+			mqtt.pub(r.c_str());
+		});
+	}
+	void out(const char *format, ...) { 
+		va_list args;
+		va_start(args, format);
+		char buf[256];
+		vsnprintf(buf, sizeof(buf), format, args);
+		va_end(args);
+		mqtt.pub(buf);
+		Serial.println(buf);
+		//jw.udpDebug(buf);
+	}
+	void out(String s) { out(s.c_str()); }
+	void out(std::string s) { out(s.c_str()); }
+};
+
+#endif
+
+class TempSensor { 
+	public:
+	#define OWNG OneWireNg_CurrentPlatform 
+	OWNG *ow = NULL; 
+	TempSensor(OWNG *p) : ow(p) {}
+	TempSensor(int pin, bool pullup = true) {
+		ow = new OWNG(pin, pullup);
+	}
+	float readTemp() { 
+			std::vector<DsTempData> t = readTemps(ow);
+			return t.size() > 0 ? t[0].degC : 0;
+	}
+};
+
+class PwmChannel {
+	int pin; 
+	int channel;
+	int pwm = -1;
+	int gradual;
+public:		
+	PwmChannel(int p, int hz = 50, int c = 0, int g = 0) : pin(p), channel(c), gradual(g) {
+		ledcSetup(channel, hz, 16);
+		ledcAttachPin(pin, channel);
+	}
+	void setMs(int p) { set(p * 4715 / 1500); };
+	void setPercent(int p) { set(p * 65535 / 100); } 
+	void set(int p) { 
+		while(gradual && pwm != -1 && pwm != p) { 
+			ledcWrite(channel, pwm);  
+			pwm += pwm < p ? 1 : -1;
+			delay(gradual);
+		}
+		ledcWrite(channel, p); 
+		pwm = p; 
+	} 
+	float get() { return (float)pwm / 65535; } 
+};
+
+void digitalToggle(int pin) { pinMode(pin, OUTPUT); digitalWrite(pin, !digitalRead(pin)); }
+
+
+
 
 //#endif
 #endif //#ifndef INC_JIMLIB_H
